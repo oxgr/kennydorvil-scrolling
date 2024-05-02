54d164736a04b8b3cddc6e882359dfbad0538dfe
ox <75142042+oxgr@users.noreply.github.com>
2024-05-02 18:10:39 -0400

    blur when enter viewport

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,71 @@
 import "./style.css";
 
-const vignettes = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-];
+main();
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="vignettes"></div>
+function main() {
+  const debugText = document.querySelector("#debugText")!;
+  const debugBtn = document.querySelector("#debugBtn")!;
 
-`;
+  // const app = document.querySelector<HTMLDivElement>("#app")!;
+  const vignettes = document.querySelector<HTMLDivElement>(".vignettes")!;
 
-for (const path of vignettes) {
-  document.querySelector<HTMLDivElement>(".vignettes")!.innerHTML += `
-    <div class="vignette">
+  const intersection = document.querySelector(".intersection")!;
+
+  const vignetteElements = addElements(vignettes);
+  const io = registerIO(intersection, debugText);
+
+  vignetteElements.forEach((vignette) => {
+    io.observe(vignette);
+  });
+
+  debugBtn.addEventListener("click", (_) => {
+    io.takeRecords();
+  });
+}
+
+function addElements(container: Element) {
+  const vignettePaths = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+  ];
+
+  vignettePaths.forEach((path, i) => {
+    container.innerHTML += `
+    <div id="${++i} "class="vignette">
       <img src="/images/${path}">
     </div>
   `;
+  });
+
+  return document.querySelectorAll(".vignette");
+}
+
+function registerIO(root: Element, debug: Element) {
+  // Register IntersectionObserver
+  return new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        console.log({
+          entry,
+          target: entry.target,
+          id: entry.target.id,
+          ratio: entry.intersectionRatio,
+        });
+        if (entry.intersectionRatio > 0.5) {
+          // Add 'active' class if observation target is inside viewport
+          entry.target.classList.add("active");
+          debug.innerHTML = `#${entry.target.id.toString()} -> ${entry.target.classList.toString()}`;
+        } else {
+          // Remove 'active' class otherwise
+          entry.target.classList.remove("active");
+        }
+      });
+    },
+    { root: null, threshold: [0.2, 0.5] },
+  );
 }
