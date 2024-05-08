f28f60b27c6ad249b57cbde1452700401a9d36d3
ox <75142042+oxgr@users.noreply.github.com>
2024-05-08 16:28:55 -0400

    cargo refactor

---
+++ src/cargo.js
@@ -0,0 +1,48 @@
+export function main() {
+  const io = registerIO();
+
+  const linkedElements = document.querySelectorAll(".linked");
+
+  linkedElements.forEach((vignette) => {
+    io.observe(vignette);
+  });
+}
+
+function registerIO() {
+  const threshold = Array(100)
+    .fill(0)
+    .map((_, i) => i * 0.01);
+
+  const ioOptions = { root: null, rootMargin: "-10% 0% -10% 0%", threshold };
+
+  const ioCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        console.log("entry");
+        // console.log({
+        //   entry,
+        //   target: entry.target,
+        //   id: entry.target.id,
+        //   ratio: entry.intersectionRatio,
+        // });
+      }
+
+      /** adjust ratio behavior **/
+      const RATIO_CORRECTION_FACTOR = 1.2;
+      const ratio = entry.intersectionRatio * RATIO_CORRECTION_FACTOR;
+      // const ratio = Math.pow(entry.intersectionRatio, 0.5);
+      // console.log({ int: entry.intersectionRatio, ratio });
+
+      /** blur effect **/
+      const BLUR_PX_MAX = 20;
+      const blurAmt = BLUR_PX_MAX - Math.round(ratio * BLUR_PX_MAX);
+      entry.target.style.filter = `blur(${blurAmt}px)`;
+
+      /** mech effect **/
+      // const ROTATE_MAX = 0.5;
+      // const rotateAmt = ROTATE_MAX - ratio * ROTATE_MAX;
+      // entry.target.style.transform = `rotateX(${rotateAmt}turn)`;
+    });
+  };
+  return new IntersectionObserver(ioCallback, ioOptions);
+}
