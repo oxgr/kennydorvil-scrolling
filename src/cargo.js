b62259c79f93d5e97d5e56847d5935942142bf01
ox <75142042+oxgr@users.noreply.github.com>
2024-05-11 02:24:13 -0400

    mech rotate

--- src/cargo.js
+++ src/cargo.js
@@ -1,14 +1,26 @@
+const debug = addDebugElement(document.body);
 main();
 
 function main() {
   console.log("main running from develop!");
 
-  const debugElement = addDebugElement(document.body);
-  const vignetteEffectElement = addVignetteEffectElement(document.body);
-  const intersectionMarkerElement = addIntersectionElement(document.body);
   const vignetteContainer = document.querySelector(".vignetteContainer");
 
+  const mainParentElement = vignetteContainer.parentNode;
+
+  const vignetteEffectElement = addVignetteEffectElement(document.body);
+  const intersectionMarkerElement = addIntersectionElement(document.body);
+
   const linkedElements = document.querySelectorAll(".linked");
+
+  linkedElements.forEach((linkedElement) => {
+    const vignetteElement = addElement(vignetteContainer, "div", {
+      id: linkedElement.href,
+      classList: ["vignette"],
+    });
+
+    vignetteElement.append(linkedElement);
+  });
 
   const io = createIntersectionObserver(vignetteContainer);
 
@@ -23,16 +35,22 @@ function main() {
 
 function createIntersectionObserver(rootElement) {
   // Rate at which the following callback function is called.
-  const THRESHOLD_RATE = 20;
+  const THRESHOLD_RATE = 100;
+  const ROOT_MARGIN_TOP = 20;
+  const ROOT_MARGIN_BOT = 30;
+  const CENTER_CUTOFF_Y = 80;
 
   // Max strength of blur in pixels
   const BLUR_STRENGTH_MAX = 20;
 
   // If element is above this value, set to max (1.0 = whole element is visible)
-  const MIN_VISIBILITY_THRESHOLD = 0.7;
+  const BLUR_MIN_VISIBILITY_THRESHOLD = 0.7;
+  const MECH_MIN_VISIBILITY_THRESHOLD = 1.0;
 
   //
-  const ROTATE_MAX = 0.25;
+  const MECH_ROTATE_MAX = 0.2;
+  const MECH_PERSPECTIVE_AMT = 80;
+  const MECH_SCALE_FACTOR = 0.2;
 
   // Fill an array with evenly dispersed values normalised 0.0-1.0
   // Points are used to trigger at what visibility points the callback function is triggered.
@@ -46,7 +64,7 @@ function createIntersectionObserver(rootElement) {
   const ioOptions = {
     root: null,
     // root: rootElement,
-    rootMargin: "-20% 0% -20% 0%",
+    rootMargin: `-${ROOT_MARGIN_TOP}% 0% -${ROOT_MARGIN_BOT}% 0% `,
     threshold: intersectionThresholds,
   };
 
@@ -54,22 +72,49 @@ function createIntersectionObserver(rootElement) {
   const ioCallback = (entries) => {
     entries.forEach((entry) => {
       const elem = entry.target;
+      console.log(entry);
+
+      const rb = entry.rootBounds;
+      const eb = entry.boundingClientRect;
+
+      const rbMidY = rb.top + rb.height * 0.5;
+      const ebAtCenterY = rbMidY - eb.height * 0.5;
+      const IS_CENTER_CORRECTION = 100;
+
+      const initRatio = entry.intersectionRatio;
 
       // if (elem.href == "07-aw-adieu") console.log({ elem: entry });
 
       // Set ratio to max if element is above minimum visibility threshold.
-      const ratio =
-        entry.intersectionRatio > MIN_VISIBILITY_THRESHOLD
-          ? 1
-          : entry.intersectionRatio;
+      const blurRatio =
+        initRatio > BLUR_MIN_VISIBILITY_THRESHOLD ? 1 : initRatio;
 
       // Set blur effect as a ratio of how visible an element is.
-      const blurAmt = BLUR_STRENGTH_MAX - Math.round(ratio * BLUR_STRENGTH_MAX);
-      entry.target.style.filter = `blur(${blurAmt}px)`;
+      // const blurAmt = BLUR_STRENGTH_MAX - Math.round(blurRatio * BLUR_STRENGTH_MAX);
+      // entry.target.style.filter = `blur(${blurAmt}px)`;
 
+      const rotateRatio =
+        initRatio > MECH_MIN_VISIBILITY_THRESHOLD ? 1 : initRatio;
       /** mech effect **/
-      // const rotateAmt = ROTATE_MAX - ratio * ROTATE_MAX;
-      // entry.target.style.transform = `rotateX(${rotateAmt}turn)`;
+      const rotateAmtBase = MECH_ROTATE_MAX - rotateRatio * MECH_ROTATE_MAX;
+      const bounds = entry.target.getBoundingClientRect();
+      const isLow = bounds.y > CENTER_CUTOFF_Y;
+      const rotateAmt = isLow ? rotateAmtBase * -1 : rotateAmtBase;
+      // const perspeAmt = 500 - rotateRatio * 500;
+      const scaleAmt = initRatio * MECH_SCALE_FACTOR + (1 - MECH_SCALE_FACTOR);
+      entry.target.style.transform = `perspective(${MECH_PERSPECTIVE_AMT}vw) rotateX(${rotateAmt}turn)  scale(${scaleAmt})`;
+      // entry.target.style.transform = `rotateX(${rotateAmt}turn)  `;
+
+      const d = {
+        rotateAmt,
+        initRatio,
+      };
+      const dStr = JSON.stringify(d, null, 2).replaceAll('"', "");
+
+      const e = entries.map((e) => e.target.getBoundingClientRect().y);
+      const eStr = JSON.stringify(e, null, 2).replaceAll('"', "");
+
+      debug.innerHTML = `${dStr}\n${eStr}`;
     });
   };
 
@@ -99,7 +144,7 @@ function addVignetteEffectElement(container) {
 }
 
 function addDebugElement(container) {
-  return addElement(container, "div", {
+  return addElement(container, "pre", {
     id: "debug",
   });
 }
