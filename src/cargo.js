decedd02c4464979c7f0d65ce12c0e7951d8ec25
ox <75142042+oxgr@users.noreply.github.com>
2024-05-25 00:23:59 -0400

    refactor constants to Params obj

--- src/cargo.js
+++ src/cargo.js
@@ -1,4 +1,32 @@
-const debugElement = addDebugElement(document.body);
+const Params = {
+  DEBUG: false,
+
+  ROOT_MARGIN_TOP_DESKTOP: 5,
+  ROOT_MARGIN_BOT_DESKTOP: 20,
+  ROOT_MARGIN_TOP_MOBILE: 25,
+  ROOT_MARGIN_BOT_MOBILE: 30,
+
+  MOBILE_WIDTH_CUTOFF: 640,
+  MOBILE_TOP_PADDING_VH: 40,
+
+  // Rate at which the following callback function is called.
+  THRESHOLD_RATE: 100,
+  CENTER_CUTOFF_Y: 60,
+
+  OPACITY_MIN: 0,
+
+  // Max strength of blur in pixels
+  BLUR_STRENGTH_MAX: 20,
+
+  //
+  MECH_ROTATE_MAX: 0.2,
+  MECH_PERSPECTIVE_AMT: 40,
+  MECH_SCALE_AMT: 0.3,
+};
+
+const debugElement = Params.DEBUG ? addDebugElement(document.body) : undefined;
+const debug = {};
+
 main();
 
 function main() {
@@ -11,17 +39,16 @@ function main() {
   addMainElements({ scriptElement, linkedElements });
 
   /* mobile checks */
-  const MOBILE_WIDTH_CUTOFF = 640;
-  const TOP_PADDING_VH = 40;
   const viewportWidth = document.documentElement.clientWidth;
-  const isMobile = viewportWidth < MOBILE_WIDTH_CUTOFF;
+  const isMobile = viewportWidth < Params.MOBILE_WIDTH_CUTOFF;
   if (isMobile) {
-    vignetteContainer.style.paddingTop = `${TOP_PADDING_VH}vh`;
+    vignetteContainer.style.paddingTop = `${Params.MOBILE_TOP_PADDING_VH}vh`;
   }
 
   /* intersection observer logic */
   const io = createIntersectionObserver({ isMobile });
 
+  // intersection observer listens to movements of each vignette
   linkedElements.forEach((vignette) => {
     io.observe(vignette);
 
@@ -36,44 +63,23 @@ function resetCSS(element) {
 }
 
 function createIntersectionObserver({ isMobile }) {
-  // General approximation of bottom nav bar
-  // desktop ~= 90-100px
-  // mobile ~= 70-80px
-  const NAVBAR_HEIGHT = 100;
+  const rootMarginTop = isMobile
+    ? Params.ROOT_MARGIN_TOP_MOBILE
+    : Params.ROOT_MARGIN_TOP_DESKTOP;
+  const rootMarginBottom = isMobile
+    ? Params.ROOT_MARGIN_BOT_MOBILE
+    : Params.ROOT_MARGIN_BOT_DESKTOP;
 
-  const ROOT_MARGIN_TOP = isMobile ? 25 : 5;
-  const ROOT_MARGIN_BOT = isMobile ? 30 : 20;
-
-  // Rate at which the following callback function is called.
-  const THRESHOLD_RATE = 100;
-  const CENTER_CUTOFF_Y = 60;
-
-  const OPACITY_MIN = 0;
-
-  // Max strength of blur in pixels
-  const BLUR_STRENGTH_MAX = 20;
-
-  // If element is above this value, set to max (1.0 = whole element is visible)
-  const BLUR_MIN_VISIBILITY_THRESHOLD = 1;
-  const MECH_MIN_VISIBILITY_THRESHOLD = 1;
-
-  //
-  const MECH_ROTATE_MAX = 0.2;
-  const MECH_PERSPECTIVE_AMT = 40;
-  const MECH_SCALE_AMT = 0.3;
-  const MECH_SCALE_OFFSET = 1 - MECH_SCALE_AMT;
-
-  // Fill an array with evenly dispersed values normalised 0.0-1.0
+  // Fill an array with evenly dispersed normalised values (0.0-1.0)
   // Points are used to trigger at what visibility points the callback function is triggered.
-  const intersectionThresholds = Array(THRESHOLD_RATE)
+  const intersectionThresholds = Array(Params.THRESHOLD_RATE)
     .fill(0)
-    .map((_, i) => i / THRESHOLD_RATE);
+    .map((_, index) => index / Params.THRESHOLD_RATE);
 
   // Options for IntersectionObserver, `null` root uses viewport
   const ioOptions = {
     root: null,
-    // root: rootElement,
-    rootMargin: `-${ROOT_MARGIN_TOP}% 0% -${ROOT_MARGIN_BOT}% 0% `,
+    rootMargin: `-${rootMarginTop}% 0% -${rootMarginBottom}% 0% `,
     threshold: intersectionThresholds,
   };
 
@@ -82,38 +88,16 @@ function createIntersectionObserver({ isMobile }) {
   // Function that is called when an element crosses a point in intersectionThresholds
   function ioCallback(entries) {
     entries.forEach((entry) => {
-      let debug = {};
-      // const elem = entry.target;
-      // console.log(entry);
-
-      const viewportCenterY = entry.rootBounds.height * 0.5;
       const viewportTop = entry.rootBounds.top;
       const boundRectTop = entry.boundingClientRect.top;
-      const boundHeightHalf = entry.boundingClientRect.height * 0.5;
       const isHigh = boundRectTop < viewportTop;
 
       const intRatio = entry.intersectionRatio;
 
-      // console.log({
-      //   id: entry.target.href,
-      //   entry,
-      //   intRatio,
-      //   intRatioY,
-      //   viewportCenterY,
-      // });
-
-      // if (boundHeight == 0) {
-      //   console.log(entry);
-      //   // console.log(entry.target.href);
-      //   ratio = 0;
-      // }
-
-      // console.log(intRatio);
-      // if (!entry.isIntersecting) {
-      //   resetCSS(entry.target);
-      //   console.log("reset CSS", entry.target.href);
-      //   return;
-      // }
+      if (Params.DEBUG) {
+        debug.boundRect = entry.boundingClientRect;
+        debug.viewport = entry.rootBounds;
+      }
 
       const filterBlurString = filterBlur(intRatio);
       const transformMechString = transformMech(intRatio, isHigh);
@@ -123,62 +107,48 @@ function createIntersectionObserver({ isMobile }) {
       entry.target.style.transform = transformMechString;
       // entry.target.style.opacity = opacityString;
 
-      // entry.target.classList.add("spin");
-      // if (initRatio < 0.1) {
-      //   entry.target.classList.remove("spin");
-      //   // entry.target.style.animation = "none";
-      // }
-
-      if (entry.target.href == "04-yseult") {
+      if (Params.DEBUG && entry.target.href == "04-yseult") {
         const debugStr = JSON.stringify(debug, null, 2).replaceAll('"', "");
-        // const entryY = entries.map((e) => e.target.getBoundingClientRect().y);
-        // const entryStr = JSON.stringify(entryY, null, 2).replaceAll('"', "");
-        // debugElement.innerHTML = `${debugStr}\n${entryStr}`;
         debugElement.innerHTML = `${debugStr}`;
       }
 
       return;
 
       function opacity(initRatio) {
-        const opacityAmt = initRatio * (1 - OPACITY_MIN) + OPACITY_MIN;
+        const opacityAmt =
+          initRatio * (1 - Params.OPACITY_MIN) + Params.OPACITY_MIN;
         return `${opacityAmt}`;
       }
 
-      function filterBlur(initRatio) {
-        // Set ratio to max if element is above minimum visibility threshold.
-        const blurRatio =
-          initRatio > BLUR_MIN_VISIBILITY_THRESHOLD ? 1 : initRatio;
-
+      function filterBlur(ratio) {
         // Set blur effect as a ratio of how visible an element is.
         const blurAmt =
-          BLUR_STRENGTH_MAX - Math.round(blurRatio * BLUR_STRENGTH_MAX);
-        return `blur(${blurAmt}px) brightness(${opacity(blurRatio) * 100}%)`;
+          Params.BLUR_STRENGTH_MAX -
+          Math.round(ratio * Params.BLUR_STRENGTH_MAX);
+        return `blur(${blurAmt}px) brightness(${opacity(ratio) * 100}%)`;
       }
 
-      function transformMech(initRatio, isHigh) {
-        const mechRatio =
-          initRatio > MECH_MIN_VISIBILITY_THRESHOLD ? 1 : initRatio;
+      function transformMech(ratio, isHigh) {
+        if (ratio == 1) return "";
 
-        if (mechRatio == 1) return "";
+        const perspectiveAmt = Params.MECH_PERSPECTIVE_AMT;
 
-        /** mech effect **/
-        const rotateAmtBase = MECH_ROTATE_MAX - mechRatio * MECH_ROTATE_MAX;
-        // const isLow = eb.y > CENTER_CUTOFF_Y;
-        // const rotateAmt = isLow ? rotateAmtBase * -1 : rotateAmtBase;
-
+        const rotateAmtBase =
+          Params.MECH_ROTATE_MAX - ratio * Params.MECH_ROTATE_MAX;
         const rotateAmt = isHigh ? rotateAmtBase : -rotateAmtBase;
-        const scaleAmt = MECH_SCALE_OFFSET + mechRatio * MECH_SCALE_AMT;
-        // const scaleAmt = 1;
 
-        debug.rotateAmtBase = rotateAmtBase;
-        debug.rotateAmt = rotateAmt;
-        debug.rotateRatio = mechRatio;
-        debug.scaleAmt = scaleAmt;
-        debug.initRatio = initRatio;
+        const scaleOffset = 1 - Params.MECH_SCALE_AMT;
+        const scaleAmt = scaleOffset + ratio * Params.MECH_SCALE_AMT;
 
-        return `perspective(${MECH_PERSPECTIVE_AMT}vw) rotateX(${rotateAmt}turn) scale(${scaleAmt})  `;
-        // return `rotateX(${rotateAmt}turn)  `;
-        //
+        if (Params.DEBUG) {
+          debug.rotateAmtBase = rotateAmtBase;
+          debug.rotateAmt = rotateAmt;
+          debug.rotateRatio = ratio;
+          debug.scaleAmt = scaleAmt;
+          debug.initRatio = ratio;
+        }
+
+        return `perspective(${perspectiveAmt}vw) rotateX(${rotateAmt}turn) scale(${scaleAmt})  `;
       }
     });
   }
@@ -191,9 +161,10 @@ function addMainElements({ scriptElement, linkedElements }) {
     classList: ["vignetteContainer"],
   });
 
-  const mainParentElement = vignetteContainer.parentNode;
-  const vignetteEffectElement = addVignetteEffectElement(document.body);
-  const intersectionMarkerElement = addIntersectionElement(document.body);
+  // These elements need to layer on global viewport
+  // and this is easier to do with `document.body`
+  addVignetteEffectElement(document.body);
+  addIntersectionElement(document.body);
 
   linkedElements.forEach((linkedElement) => {
     // add vignette elements into container
