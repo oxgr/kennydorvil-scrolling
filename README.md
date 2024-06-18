b7bc5b193c5f4b14db373021bd5b9d5d7f77dbd6
ox <75142042+oxgr@users.noreply.github.com>
2024-06-17 23:00:27 -0400

    docs: comments + cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scrolling effects for kennydorvil.com
+
+This repo contains effects commissioned by Allen's Cruz - designer of the portfolio website for Kenny Dorvil.
--- src/cargo.js
+++ src/cargo.js
@@ -1,3 +1,4 @@
+import { Params } from "./params.js";
 import { createIntersectionObserver } from "./observer.js";
 import { addVignetteEffectElement } from "./element.js";
 import { resetCSS } from "./utils.js";
@@ -13,6 +14,8 @@ function main() {
 
   // Visual vignette element needs to layer on global viewport
   // and this is easier to do with `document.body`
+  // NOTE: This feature was disabled for legibility of captions.
+  //
   // addVignetteEffectElement(document.body);
 
   // Create an observer that watches the movements of elements within its bounds
@@ -22,6 +25,11 @@ function main() {
   vignetteElements.forEach((vignette) => {
     // io.observe(vignette.shadowRoot.querySelector(".media"));
     io.observe(vignette);
+
+    // Set a custom class name to prep caption fade in if enabled.
+    if (Params.CAPTION_FADEIN_ENABLE) {
+      vignette.querySelector(".caption")?.classList.add("fadeIn");
+    }
 
     // Manually unset CSS variables on init to counteract inital sets
     resetCSS(vignette);
