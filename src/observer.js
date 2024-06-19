6f5c4a6e62e86f563796258061c0a8f38ee96d47
ox <75142042+oxgr@users.noreply.github.com>
2024-06-18 22:47:34 -0400

    chore: clean up observer logic

--- src/observer.js
+++ src/observer.js
@@ -1,7 +1,9 @@
 import { Params } from "./params.js";
-import { debug } from "./debug.js";
 import { applyVignetteCss, getIsMobile, getVignetteImg } from "./utils.js";
 
+/**
+ * Instantiates an IntersectionObserver object with the necessary parameters and callback function.
+ */
 export function createIntersectionObserver(container) {
   const isMobile = getIsMobile(container);
 
@@ -30,84 +32,79 @@ export function createIntersectionObserver(container) {
   };
 
   return new IntersectionObserver(ioCallback, ioOptions);
+}
 
-  /**
-   * Function that is called every an element crosses a point in `ioOptions.threshold`
-   */
-  function ioCallback(entries) {
-    entries.forEach((entry) => {
-      const vignette = entry.target;
-      if (!getVignetteImg(vignette)) return;
+/**
+ * Function that is called every an element crosses a point in `ioOptions.threshold`
+ */
+function ioCallback(entries) {
+  entries.forEach(entryHandler);
+  return;
 
-      // How much the element is visible within the intersection bounds
-      const intRatio = entry.intersectionRatio;
+  function entryHandler() {
+    const vignette = entry.target;
 
-      // Determine whether the element is above or below the focused center
-      const viewportTop = entry.rootBounds.top;
-      const boundRectTop = entry.boundingClientRect.top;
-      const isHigh = boundRectTop < viewportTop;
+    // Guard for media-items outside of home page
+    if (!getVignetteImg(vignette)) return;
 
-      // Get the relevant CSS strings to apply
-      const filterBlurString = filterBlur(intRatio);
-      const transformMechString = transformMech(intRatio, isHigh);
-      // const opacityString = opacity(initRatio);
+    // How much the element is visible within the intersection bounds
+    const intRatio = entry.intersectionRatio;
 
-      // Apply the CSS strings to the style
-      try {
-        applyVignetteCss(vignette, {
-          filter: filterBlurString,
-          transform: transformMechString,
-        });
-      } catch (err) {
-        console.error(err);
-      }
+    // Determine whether the element is above or below the focused center
+    const isHigh = entry.boundingClientRect.top < entry.rootBounds.top;
 
-      // Fade in captions
-      if (Params.CAPTION_FADEIN_ENABLE) {
-        const captionElement = vignette.querySelector(".caption");
-        if (captionElement) {
-          captionElement.style.filter = filterBlurString;
-          if (intRatio > Params.CAPTION_FADEIN_THRESHOLD) {
-            captionElement.classList.add("captionVisible");
-          } else {
-            captionElement.classList.remove("captionVisible");
-          }
+    // Get the relevant CSS strings to apply
+    const filterBlurString = filterBlur(intRatio);
+    const transformMechString = transformMech(intRatio, isHigh);
+    // const opacityString = opacity(initRatio);
+
+    // Apply the CSS strings to the style
+    try {
+      applyVignetteCss(vignette, {
+        filter: filterBlurString,
+        transform: transformMechString,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+
+    // Fade in captions
+    if (Params.CAPTION_FADEIN_ENABLE) {
+      const captionElement = vignette.querySelector(".caption");
+      if (captionElement) {
+        captionElement.style.filter = filterBlurString;
+        if (intRatio > Params.CAPTION_FADEIN_THRESHOLD) {
+          captionElement.classList.add("captionVisible");
+        } else {
+          captionElement.classList.remove("captionVisible");
         }
       }
-
-      if (Params.DEBUG && vignette.href?.includes("02")) {
-        debug.print();
-      }
-
-      return;
-
-      function opacity(ratio) {
-        const opacityAmt =
-          ratio * (1 - Params.OPACITY_MIN) + Params.OPACITY_MIN;
-        return `${opacityAmt}`;
-      }
-
-      function filterBlur(ratio) {
-        const blurAmt =
-          Params.BLUR_STRENGTH_MAX -
-          Math.round(ratio * Params.BLUR_STRENGTH_MAX);
-        return `blur(${blurAmt}px) brightness(${opacity(ratio) * 100}%)`;
-      }
-
-      function transformMech(ratio, isHigh) {
-        if (ratio == 1) return "";
-
-        const perspectiveAmt = Params.MECH_PERSPECTIVE_AMT;
-
-        const rotateAmtBase =
-          Params.MECH_ROTATE_MAX - ratio * Params.MECH_ROTATE_MAX;
-        const rotateAmt = isHigh ? rotateAmtBase : -rotateAmtBase;
-
-        const scaleOffset = 1 - Params.MECH_SCALE_AMT;
-        const scaleAmt = scaleOffset + ratio * Params.MECH_SCALE_AMT;
-
-        return `perspective(${perspectiveAmt}vw) rotateX(${rotateAmt}turn) scale(${scaleAmt})  `;
-      }
-    });
+    }
+    return;
   }
+}
+
+function opacity(ratio) {
+  const opacityAmt = ratio * (1 - Params.OPACITY_MIN) + Params.OPACITY_MIN;
+  return `${opacityAmt}`;
+}
+
+function filterBlur(ratio) {
+  const blurAmt =
+    Params.BLUR_STRENGTH_MAX - Math.round(ratio * Params.BLUR_STRENGTH_MAX);
+  return `blur(${blurAmt}px) brightness(${opacity(ratio) * 100}%)`;
+}
+
+function transformMech(ratio, isHigh) {
+  if (ratio == 1) return "";
+
+  const perspectiveAmt = Params.MECH_PERSPECTIVE_AMT;
+
+  const rotateAmtBase = Params.MECH_ROTATE_MAX - ratio * Params.MECH_ROTATE_MAX;
+  const rotateAmt = isHigh ? rotateAmtBase : -rotateAmtBase;
+
+  const scaleOffset = 1 - Params.MECH_SCALE_AMT;
+  const scaleAmt = scaleOffset + ratio * Params.MECH_SCALE_AMT;
+
+  return `perspective(${perspectiveAmt}vw) rotateX(${rotateAmt}turn) scale(${scaleAmt})  `;
 }
